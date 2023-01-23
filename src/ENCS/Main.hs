df6ad0b366b200b6d0afa736d174b5fc3befd63e
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-01-23 10:16:18 +0300

    Update deps

--- src/ENCS/Main.hs
+++ src/ENCS/Main.hs
@@ -1,40 +1,35 @@
-{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE EmptyCase         #-}
-{-# LANGUAGE EmptyDataDeriving #-}
-{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeApplications  #-}
 {-# LANGUAGE TypeFamilies      #-}
 
 module ENCS.Main where
 
-import Control.Monad             (void)
-import Control.Monad.Reader      (asks)
-import Control.Monad.Catch       (Exception)
-import Data.Aeson                (FromJSON(..))
-import Data.Functor              ((<&>))
-import Data.List                 (tails)
+import Cardano.Server.Config             (decodeOrErrorFromFile)
+import Cardano.Server.Input              (InputContext(..))
+import Cardano.Server.Internal           (HasServer(..), Env(..), runAppM)
+import Cardano.Server.Tx                 (mkTx)
+import Control.Monad                     (void)
+import Control.Monad.Reader              (asks)
+import Data.Aeson                        (FromJSON(..))
+import Data.Foldable                     (traverse_)
+import Data.Functor                      ((<&>))
+import Data.List                         (tails)
 import ENCS.Opts
-import ENCOINS.ENCS.Distribution (mkDistribution, processDistribution)
-import ENCOINS.ENCS.OffChain     (encsCurrencySymbol, encsMintTx, distributionTx)
-import ENCOINS.ENCS.OnChain      (ENCSParams, DistributionValidatorParams, distributionFee, distributionFeeCount)
-import GHC.Generics              (Generic)
-import Plutus.V2.Ledger.Api      (Address)
-import Servant                   (NoContent)
-import Server.Config             (decodeOrErrorFromFile)
-import Server.Endpoints.SubmitTx (HasSubmitTxEndpoint(..))
-import Server.Internal           (HasServer(..), Env(..), SetupM, runSetupM, loadEnv)
-import Server.Tx                 (mkTx)
+import ENCOINS.ENCS.Distribution         (mkDistribution, processDistribution)
+import ENCOINS.ENCS.OffChain             (encsMintTx, distributionTx)
+import ENCOINS.ENCS.OnChain              (ENCSParams, distributionFee, distributionFeeCount)
+import GHC.Generics                      (Generic)
+import IO.Wallet                         (ownAddresses)
+import Plutus.V2.Ledger.Api              (Address)
 
 runENCSApp :: IO ()
 runENCSApp = do
     mode <- runWithOpts
-    env <- loadEnv
-    runSetupM @ENCSApp env $ case mode of
-        Run   -> cycleTx
-        Setup -> setupServer
+    runAppM @ENCSApp $ case mode of
+        Run   -> serverIdle
+        Setup -> serverSetup
 
 data ENCSApp
 
@@ -52,20 +47,21 @@ instance HasServer ENCSApp where
         envAddrList   <- decodeOrErrorFromFile "testnet/addresses.json" <&> processDistribution
         pure ENCSEnv{..}
 
-    type RedeemerOf ENCSApp = ()
+    type InputOf ENCSApp = ()
 
-    getCurrencySymbol = encsCurrencySymbol <$> asks (envENCSParams . envAuxiliary)
-
-    processTokens _ = pure ()
-
-    setupServer = do
+    serverSetup = do
         ENCSEnv{..} <- asks envAuxiliary
         let amtF = distributionFee * distributionFeeCount
             distribution = mkDistribution envENCSParams envAddrList amtF
-        void $ mkTx [] [encsMintTx envENCSParams distribution]
+        addrs <- ownAddresses
+        void $ mkTx addrs emptyContext [encsMintTx envENCSParams distribution]
 
-    cycleTx = do
+    serverIdle = do
         ENCSEnv{..} <- asks envAuxiliary
         let amtF = distributionFee * distributionFeeCount
             distribution = mkDistribution envENCSParams envAddrList amtF
-        void $ traverse (\d -> mkTx [] [distributionTx d]) $ tails distribution
+        addrs <- ownAddresses
+        traverse_ (\d -> mkTx addrs emptyContext [distributionTx d]) $ tails distribution
+
+emptyContext :: InputContext
+emptyContext = InputContextServer mempty
