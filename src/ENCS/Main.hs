f4dfd4792c0d1f2bd307c3d609c02af22a4502d0
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-02-17 13:12:43 +0300

    Add completed distribution message

--- src/ENCS/Main.hs
+++ src/ENCS/Main.hs
@@ -8,31 +8,33 @@
 
 module ENCS.Main where
 
-import           Control.Monad                   (unless, void)
-import           Control.Monad.Reader            (asks)
-import           Data.Aeson                      (FromJSON (..), eitherDecodeFileStrict)
-import           Data.Default                    (def)
-import           Data.Functor                    ((<&>))
-import           Data.List                       (tails)
-import           GHC.Generics                    (Generic)
-import           Plutus.V2.Ledger.Api            (Address)
-import qualified PlutusTx.Prelude                as Plutus
-import           Cardano.Api                     (writeFileJSON)
-import           Cardano.Server.Config           (decodeOrErrorFromFile)
-import           Cardano.Server.Internal         (AppM, Env (..), HasServer (..), getNetworkId, runAppM)
-import           Cardano.Server.Tx               (checkForCleanUtxos, mkTx)
-import           Cardano.Server.Utils.ChainIndex (ChainIndex (Kupo))
-import           Control.Monad.IO.Class          (MonadIO (..))
-import           Data.Maybe                      (fromMaybe)
-import qualified Data.Text                       as T
-import qualified Data.Text.IO                    as T
-import           ENCOINS.ENCS.Distribution       (mkDistribution, processDistribution)
-import           ENCOINS.ENCS.Distribution.IO    (verifyDistribution)
-import           ENCOINS.ENCS.OffChain           (distributionTx, encsMintTx)
-import           ENCOINS.ENCS.OnChain            (ENCSParams, distributionValidatorAddresses)
-import           ENCS.Opts                       (ServerMode (..), runWithOpts)
-import           PlutusAppsExtra.IO.Wallet       (ownAddresses)
-import           PlutusAppsExtra.Utils.Address   (addressToBech32)
+import           Cardano.Api                   (writeFileJSON)
+import           Cardano.Server.Config         (decodeOrErrorFromFile)
+import           Cardano.Server.Internal       (AppM (..), Env (..), HasServer (..), getNetworkId, runAppM)
+import           Cardano.Server.Tx             (checkForCleanUtxos, mkTx)
+import           Control.Monad                 (unless, void)
+import           Control.Monad.Catch           (Exception (..), MonadThrow (throwM), handle)
+import           Control.Monad.IO.Class        (MonadIO (liftIO))
+import           Control.Monad.Reader          (asks)
+import           Data.Aeson                    (FromJSON (..), eitherDecodeFileStrict)
+import           Data.Default                  (def)
+import           Data.Functor                  ((<&>))
+import           Data.List                     (tails)
+import           Data.Maybe                    (fromMaybe)
+import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as T
+import           ENCOINS.ENCS.Distribution     (mkDistribution, processDistribution)
+import           ENCOINS.ENCS.Distribution.IO  (verifyDistribution)
+import           ENCOINS.ENCS.OffChain         (distributionTx, encsMintTx)
+import           ENCOINS.ENCS.OnChain          (ENCSParams, distributionValidatorAddresses)
+import           ENCS.Opts                     (ServerMode (Run, Setup, Verify), runWithOpts)
+import           GHC.Generics                  (Generic)
+import           Plutus.V2.Ledger.Api          (Address)
+import           PlutusAppsExtra.IO.ChainIndex (ChainIndex (..))
+import           PlutusAppsExtra.IO.Wallet     (ownAddresses)
+import           PlutusAppsExtra.Types.Error   (MkTxError (..))
+import           PlutusAppsExtra.Utils.Address (addressToBech32)
+import qualified PlutusTx.Prelude              as Plutus
 
 runENCSApp :: IO ()
 runENCSApp = do
@@ -70,15 +72,19 @@ instance HasServer ENCSApp where
             addrs <- ownAddresses
             void $ mkTx addrs def [encsMintTx envENCSParams utxos]
 
-    serverIdle = do
-        checkForCleanUtxos
-        ENCSEnv{..} <- asks envAuxiliary
-        let fee          = 100
-            nFeeCovered  = Plutus.length envAddrList
-            distribution = mkDistribution envENCSParams envAddrList (fee, nFeeCovered)
-            addrs = distributionValidatorAddresses distribution
-        void $ mkTx addrs def $ map distributionTx $ tails distribution
-        serverIdle
+    serverIdle = handle handleEnd $ do
+            checkForCleanUtxos
+            ENCSEnv{..} <- asks envAuxiliary
+            let fee          = 100
+                nFeeCovered  = Plutus.length envAddrList
+                distribution = mkDistribution envENCSParams envAddrList (fee, nFeeCovered)
+                addrs = distributionValidatorAddresses distribution
+            void $ mkTx addrs def $ map distributionTx $ init $ tails distribution
+            serverIdle
+        where
+            handleEnd e = case fromException e of
+                Just AllConstructorsFailed{} -> liftIO $ putStrLn "The distribution is completed!"
+                _                            -> throwM e
 
     defaultChainIndex = Kupo
 
